14f7c123e4aec656f51f7b462df41ae29b9a4925
filipelima26 <filipeglima@terra.com.br>
2023-06-19 13:57:59 -0300

    :tada initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// function toggleMode() {
+//   const html = document.documentElement;
+//   if (html.classList.contains('light')) {
+//     html.classList.remove('light');
+//   } else {
+//     html.classList.add('light');
+//   }
+// }
+// Ou então:
+
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle('light');
+  const foto = document.querySelector('#profile img');
+  if (html.classList.contains('light')) {
+    foto.setAttribute('src', '/assets/profile-2.png');
+  } else {
+    foto.setAttribute('src', '/assets/profile.png');
+  }
+}
